1326b306209df7c77e5526d060a90cd07462f123
osiyo <merewall@gmail.com>
2021-07-30 10:51:51 -0400

    updating the session object with cookie expiration time to 15 min

--- server.js
+++ server.js
@@ -6,14 +6,16 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
-// const helpers = require('./utils/helpers');
+const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sess = {
   secret: 'Super secret secret',
-  cookie: {},
+  cookie: {
+    expires: 900000,
+  },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
@@ -23,8 +25,8 @@ const sess = {
 
 app.use(session(sess));
 
-// const hbs = exphbs.create({ helpers });
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
+
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
