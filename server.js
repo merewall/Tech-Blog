da15af2d7bc455554b47e65c4d45af174c072abe
osiyo <merewall@gmail.com>
2021-07-31 19:08:52 -0400

    commenting out server.js file

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
+// Bringing in packages for express server, handlebars and sequelize
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+// Bringing in controllers directory routes, sequelize config, and helper functions
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const helpers = require('./utils/helpers');
 
+// Declaring variables for express server and port
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Session configuration with cookie timeout at 15minutes
 const sess = {
   secret: 'Super secret secret',
   cookie: {
@@ -23,20 +27,25 @@ const sess = {
   }),
 };
 
+// Use express-session with the session configuration
 app.use(session(sess));
 
+// Create an instance of handlebars with helpers functions
 const hbs = exphbs.create({ helpers });
 
-
+// Using and setting the template engine to handlebars for rendering VIEW
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+// Data parsing and enabling use of static files in public directory
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Enabling use of routes
 app.use(routes);
 
+// Start sequelize database listening on port
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
